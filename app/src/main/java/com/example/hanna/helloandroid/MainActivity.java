aa5394bfd849808fc328db542a809db60e210d03
Hanna <hanna.botar@teamnet.ro>
2017-11-28 19:08:02 +0200

    hide status bar

--- app/src/main/java/com/example/hanna/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/hanna/helloandroid/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.hanna.helloandroid;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        /*ActionBar actionBar = getActionBar(); -->> null
+        actionBar.hide();*/
+
         setContentView(R.layout.activity_main);
     }
 }
